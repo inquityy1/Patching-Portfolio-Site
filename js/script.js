79770136cc256dafc6284da650dbbf9aa48bf817
inquityy1 <nebojsanikolic12350@yahoo.com>
2020-11-29 23:35:07 +0100

    ending about me,experience and home sections

--- js/script.js
+++ js/script.js
@@ -2,7 +2,24 @@ $(document).ready(function(){
 	
 	//toggle button
 	$('.btn').click(function(){
-		$('.nav-links').slideToggle(3000);
+		$('.nav-links').slideToggle(500);
 		$('.btn').toggleClass('change');
+	})
+
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:5
+			}
+		}
 	})
 })
